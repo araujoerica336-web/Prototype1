64df97302a87cf6aac7129c01f1aaa8ffee75253
araujoerica336-web <araujoerica336@gmail.com>
2026-05-04 17:24:28 +0100

    Unidade 1 - Introdução

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private variables
+    private float speed = 8.0f;
+    private float turnSpeed = 25.0f;
+
+    private float horizontalInput;
+
+    private float forwardInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+      //Move the vehicle forward based on vertical input
+      transform.Translate(Vector3.forward * Time.deltaTime *speed * forwardInput);
+      //Rotates the car basede on horizontal input
+      transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
